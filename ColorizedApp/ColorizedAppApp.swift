e6b765ec978f32b8b428962139d4acaa035154b1
Andrey Zhivotov <res_a@mail.ru>
2022-06-07 16:08:14 +0300

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Andrey Zhivotov on 07.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
